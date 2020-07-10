5048d6a6cef3f0636373dddf39c3b46c713e02a2
juuhgouvea <juliasantos752@yahoo.com.br>
2020-07-10 01:39:57 -0300

    handle receive images

--- ReceiveIt/app/src/main/java/com/example/receiveit/MainActivity.kt
+++ ReceiveIt/app/src/main/java/com/example/receiveit/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.receiveit
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,14 +14,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handleReceive()
     }
 
-    fun handleReceive(){
+    private fun handleReceive(){
         if (intent.action == Intent.ACTION_SEND && intent.type != null) {
             if ("text/plain" == intent.type) {
                 val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
                 if (sharedText != null)
                     receive_message.text = sharedText
+            }
+            if ("image/*" == intent.type) {
+                val stream = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM);
+                if (stream != null)
+                    sharedImage.setImageURI(stream)
             }
         }
     }
