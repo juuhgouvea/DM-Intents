f163155774c8475a7f130460bbeeec735cd30689
juuhgouvea <juliasantos752@yahoo.com.br>
2020-11-06 18:55:09 -0300

    add Dexter and manage permissions

--- ShareIt/app/src/main/java/com/example/shareit/MainActivity.kt
+++ ShareIt/app/src/main/java/com/example/shareit/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.shareit
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,7 +39,29 @@ class MainActivity : AppCompatActivity() {
         startActivity(shareIntent)
     }
 
-    private fun handleShareImage () {
+    private fun handleShareImage (){
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object: PermissionListener{
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    handleShareImageExecute();
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText( applicationContext, "É necessário garantir permissão para acessarmos os arquivos!", Toast.LENGTH_LONG).show()
+                }
+            })
+            .check()
+    }
+
+    private fun handleShareImageExecute () {
         if(imageURI == null) return;
 
         val shareIntent = Intent(Intent.ACTION_SEND)
@@ -43,7 +73,29 @@ class MainActivity : AppCompatActivity() {
         startActivity(shareIntent)
     }
 
-    private fun handleChooseImage () {
+    private fun handleChooseImage (){
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object: PermissionListener{
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    handleChooseImageExecute();
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText( applicationContext, "É necessário garantir permissão para acessarmos os arquivos!", Toast.LENGTH_LONG).show()
+                }
+            })
+            .check()
+    }
+
+    private fun handleChooseImageExecute () {
         val galleryIntent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, PICK_IMAGE);
     }
